6be86c4aac929feedac152acc08cc40c2be561e5
Zach Mueller <muellerzr@gmail.com>
2022-08-29 09:27:32 -0400

    Include install script and aliases

---
+++ setup.sh
@@ -0,0 +1,8 @@
+wget -O ~/.bash_aliases https://raw.githubusercontent.com/muellerzr/dotfiles/master/.bash_aliases
+source activate ~/.bash_aliases
+
+[[ -z $NAME  ]] && read -e -p "Enter your name (for git configuration): " NAME
+[[ -z $EMAIL ]] && read -e -p "Enter your email (for git configuration): " EMAIL
+[[ $NAME  ]] && git config --global user.name "$NAME"
+[[ $EMAIL ]] && git config --global user.email "$EMAIL"
+git config --global credential.helper store
